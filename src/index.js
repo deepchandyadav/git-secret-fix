711ea069c370eadb1ff6b8ebc5997764d53acc73
Deepchand Yadav <deepchand.yadav@contentstack.com>
2024-07-09 17:57:46 +0530

    add some passwords

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 
 const authtoken = "blt63b7080da56f5ddd";
 const organization_uid = "blt3155bffc7360dddd";
+const access_token = "abcadfqwe134asdf1234a";
+const password = "thisismyfakepassword1241414";
+const user_password = "thisisverrrrrysecretpassword";
 
 console.log({ authtoken, organization_uid });
 
